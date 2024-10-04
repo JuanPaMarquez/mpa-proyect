db3d05c8dc95e00b65f46eee436945e7605fc3e5
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2024-10-03 22:57:15 -0500

    Initial Commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import '../styles/App.css'
+import Login from '../screens/Login'
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ src/screens/Login.tsx
@@ -0,0 +1,73 @@
+import '../styles/Login.css'
+import { useState } from 'react'
+import { FiUser, FiLock  } from "react-icons/fi";
+
+function Login() {
+  const [inputUser, setInputUser] = useState('');
+  const [inputLock, setInputLock] = useState('');
+
+  const handleInputUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputUser(e.target.value);
+  };
+
+  const handleInputLock = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputLock(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(inputUser);
+  };
+
+  return (
+    <div className='loginContainer'>
+      <div>
+        <img className='logoAccount' src="src/assets/accountLogo.png" alt="logo"/>
+      </div>
+      <h1 className='titleLogin'>Iniciar Sesión</h1>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className="input-container-user">
+          {inputUser === '' && (
+                <div className="iconUser">
+                    <FiUser />
+                </div>
+            )}
+          <input 
+            type="text" 
+            placeholder="Usuario"
+            value={inputUser}
+            onChange={handleInputUser}
+            required
+          />
+        </div>
+        <div className="input-container-user">
+          {inputLock === '' && (
+                <div className="iconUser">
+                    <FiLock />
+                </div>
+            )}
+          <input 
+            type="password" 
+            placeholder="Contraseña" 
+            value={inputLock}
+            onChange={handleInputLock}
+            required
+          />
+        </div>
+        <h3 className='textPassword'>
+          ¿Has olvidado tu <u>
+              <a 
+                href="" 
+                className='buttonPassword'
+              >contraseña
+              </a>
+            </u>?
+        </h3>
+        <button className='buttonSubmit' type="submit">INGRESAR</button>
+      </form>
+      <button className='buttonAccount'>CREAR CUENTA</button>
+    </div>
+  )
+}
+
+export default Login
