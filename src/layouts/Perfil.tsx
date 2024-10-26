0789496fa042624dbfcf9e85b22f1eb40ce385a2
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2024-10-26 14:32:54 -0500

    se agrego el panel de historial y demas botones

--- src/layouts/Perfil.tsx
+++ src/layouts/Perfil.tsx
@@ -1,5 +1,7 @@
 import { IoPersonOutline } from "react-icons/io5";
 import { IoMdSettings } from "react-icons/io";
+import { MdDeleteOutline } from "react-icons/md";
+import { FaRegQuestionCircle } from "react-icons/fa";
 import "../styles/Perfil.css";
 
 function Perfil() {
@@ -14,7 +16,57 @@ function Perfil() {
             <IoMdSettings />
           </div>
         </div>
-        <div className="barraContenido">contenido</div>
+        <div className="barraContenido">
+          <table className="perfilTable">
+            <thead className="perfilTableHead">
+              <h2>Historial de predicciones</h2>
+            </thead>
+            <tbody>
+              <tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr><tr>
+                <th className="prediccion">Prediccion 1</th>
+                <th>Predicion CD AR Corte I</th>
+                <th><button className="delete"><MdDeleteOutline /></button></th>
+              </tr>
+              
+            </tbody>
+          </table>
+          <div className="pieConfig">
+            <button className="buttonInfo">Informacion<FaRegQuestionCircle /></button>
+            <button className="buttonNewPrediction">Nueva Prediccion</button>
+          </div>
+        </div>
       </main>
     </div>
   )
